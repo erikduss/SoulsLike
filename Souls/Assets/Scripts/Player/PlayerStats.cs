a5d4e70207a05dd3703135b2aa7aed6042ac2105
erikduss <erikdusschooten@gmail.com>
2023-02-15 15:02:06 +0100

    implemented basic AI detection and following

--- Souls/Assets/Scripts/Player/PlayerStats.cs
+++ Souls/Assets/Scripts/Player/PlayerStats.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 namespace SoulsLike
 {
-    public class PlayerStats : MonoBehaviour
+    public class PlayerStats : CharacterStats
     {
-        public int healthLevel = 10;
-        public int maxHealth;
-        public int currentHealth;
-
-        public int staminaLevel = 10;
-        public int maxStamina;
-        public int currentStamina;
-
         HealthBar healthBar;
         StaminaBar staminaBar;
 
